fe9cbbe44c3a5cf9d106fd58fe27067302cb39f1
Goutham <whygoutham@gmail.com>
2025-09-22 12:45:34 +0530

    linkedlist

--- main_5.py
+++ main_5.py
@@ -1,3 +1,6 @@
+
+
+
 class Node:
     def __init__(self , val):
         self.val = val
@@ -5,13 +8,53 @@ class Node:
 
 
 
-n1 = Node(1)
-n2 = Node(2)
-n3 = Node(3)
 
-print(n1.val,n2.val,n3.val)
+class Sll:
+    head = None
+    def insert_at_first(self,data):
+        newnode = Node(data)
+        if self.head is not None:
+            newnode.next = self.head
+            self.head = newnode
+        else:
+            self.head = newnode
 
-n1.next = n2
-n2.next = n3
+    def insert_at_end(self,data):
+        newnode = Node(data)
+        if self.head is None:
+            self.head = newnode
+        temp = self.head
+        while temp.next is not None:
+            temp = temp.next
+        
+        temp.next = newnode
+    
+    def reverse(self,root):
+        prev = None
+        cur = root
+        while cur is not None:
+            n = cur.next
+            cur.next = prev 
+            prev = cur
+            cur = n
+        
+        return prev
 
-print(n1.next.val)
+
+n1 = Sll()
+n1.insert_at_first(3)
+n1.insert_at_first(2)
+n1.insert_at_first(1)
+
+n1.insert_at_end(4)
+
+n1.head = n1.reverse(n1.head)
+x = n1.head
+while x is not None:
+    print(x.val)
+    x = x.next
+
+
+
+
+
