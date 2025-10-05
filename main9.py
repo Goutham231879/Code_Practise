02d843ae2b05396db251287a732ee43d949053d6
Goutham <whygoutham@gmail.com>
2025-10-05 14:33:59 +0530

    tree

---
+++ main9.py
@@ -0,0 +1,24 @@
+class Node:
+    def __init__(self , data):
+        self.data=data
+        self.left=None
+        self.right=None
+
+
+def inorder(root):
+    if root == None:
+        return
+    inorder(root.left)
+    print(root.data)
+    inorder(root.right)
+    
+
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+
+
+
+inorder(root)
